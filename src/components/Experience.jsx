dfb4546fa2b896feeb808f3139f82545448f56fb
Abrahami2 <mhamitou2022@fau.edu>
2025-01-16 12:17:08 -0500

    tweakedEx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,28 +2,18 @@ import { TypeAnimation } from "react-type-animation";
 import { useState } from "react";
 import SystemService from "./Experience/SystemService";
 import AWSComponent from "./Experience/AWSComponent";
-import NewExp from "./Experience/NewExp";
 
 const Experience = () => {
   const [workAws, setWorkAws] = useState(true);
   const [floridaAtlantic, setFloridaAtlantic] = useState(false);
-  const [anotherExp, setAnotherExp] = useState(false);
 
   const handleAppTeam = () => {
     setFloridaAtlantic(true);
-    setAnotherExp(false);
-    setWorkAws(false);
-  };
-
-  const handleBIT = () => {
-    setFloridaAtlantic(false);
-    setAnotherExp(true);
     setWorkAws(false);
   };
 
   const handleMicrosoft = () => {
     setFloridaAtlantic(false);
-    setAnotherExp(false);
     setWorkAws(true);
   };
 
@@ -70,20 +60,9 @@ const Experience = () => {
           >
             Florida Atlantic University
           </li>
-          <li
-            onClick={handleBIT}
-            className={`${
-              anotherExp
-                ? "border-l-textBlue text-textBlue"
-                : "border-l-textDark text-textDark"
-            } border-l-2 bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
-          >
-            Another Experience
-          </li>
         </ul>
         {workAws && <AWSComponent />}
         {floridaAtlantic && <SystemService />}
-        {anotherExp && <NewExp />}
       </div>
     </section>
   );
